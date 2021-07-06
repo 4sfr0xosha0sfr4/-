11d38e092b571005660b048c6b231517b361761a
sfrsfrsfr404sfrsfrsfr <86876730+sfrsfrsfr404sfrsfrsfr@users.noreply.github.com>
2021-07-06 07:34:52 +0300

    Update .insta.py

--- .insta.py
+++ .insta.py
@@ -135,6 +135,11 @@ def dwbara():
     print(logo)
     i=input("   Choese: ")
     if i=="1":
+        bad=0
+        hits=0
+        timeout=0
+        error=0
+        checkpoint=0
         instagram()
     elif i=="0":
         print(" Exit !")
