d0bb7bcc92ba6580685d7729134883226063816a
sfrsfrsfr404sfrsfrsfr <86876730+sfrsfrsfr404sfrsfrsfr@users.noreply.github.com>
2021-07-09 03:05:54 +0300

    Update .insta.py

--- .insta.py
+++ .insta.py
@@ -203,20 +203,7 @@ def instagram2():
 	import requests, random
 	from user_agent import generate_user_agent
 	from datetime import datetime
-    for lol in range(10000):
-        sys.stdout=open("combo.txt", "a")
-        r=random.randint(1000000, 9999999)
-        rr=random.randint(1000000, 9999999)
-        print("+964770"+str(r)+":"+str(r))
-        print("+964750"+str(rr)+":"+str(rr))
-	r = requests.session()
-    if agar=='y' or agar=='yes' or agar=='Y' or agar=='YES' or agar=='Yes':
-        ID=input("    Your ID Telegram :")
-        token=input("    Token(bot) : ")
-    elif iagar=='n' or agar=='no' or agar=='N' or agar=='NO' or agar=='No':
-        pass
-    else:
-        pass
+    r = requests.session()
 	print(wd+'    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 	def loopPp():
 		try:
@@ -306,19 +293,6 @@ def instagram3():
 	from user_agent import generate_user_agent
 	from datetime import datetime
 	r = requests.session()    
-    for loll in range(10000):
-        sys.stdout=open("combo.txt", "a")
-        r=random.randint(1000000, 9999999)
-        rr=random.randint(1000000, 9999999)
-        print("+964770"+str(r)+":0770"+str(r))
-        print("+964750"+str(rr)+":0750"+str(rr))
-    if agar=='y' or agar=='yes' or agar=='Y' or agar=='YES' or agar=='Yes':
-        ID=input("    Your ID Telegram :")
-        token=input("    Token(bot) : ")
-    elif iagar=='n' or agar=='no' or agar=='N' or agar=='NO' or agar=='No':
-        pass
-    else:
-        pass
 	print(wd+'    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 	def loopPp():
 		try:
@@ -514,6 +488,7 @@ def instagram4():
 ################################################################################################################################################################################################
 ################################################################################################
 ################################################################################################
+###############################################################
 def i1():
     if agar=='y' or agar=='yes' or agar=='Y' or agar=='YES' or agar=='Yes':
     ID=input("    Your ID Telegram :")
@@ -523,25 +498,52 @@ def i1():
     else:
         instgram1()
 def i2():
+    import sys, random, os
+    for lol in range(10000):
+        sys.stdout=open("combo.txt", "a")
+        r=random.randint(1000000, 9999999)
+        rr=random.randint(1000000, 9999999)
+        print("+964770"+str(r)+":"+str(r))
+        print("+964750"+str(rr)+":"+str(rr))
     if agar=='y' or agar=='yes' or agar=='Y' or agar=='YES' or agar=='Yes':
-    ID=input("    Your ID Telegram :")
-    token=input("    Token(bot) : ")
+        ID=input("    Your ID Telegram :")
+        token=input("    Token(bot) : ")
     elif iagar=='n' or agar=='no' or agar=='N' or agar=='NO' or agar=='No':
-        instgram2()
+    instgram2()
     else:
         instgram2()
 def i3():
+    for loll in range(10000):
+        sys.stdout=open("combo.txt", "a")
+        r=random.randint(1000000, 9999999)
+        rr=random.randint(1000000, 9999999)
+        print("+964770"+str(r)+":0770"+str(r))
+        print("+964750"+str(rr)+":0750"+str(rr))
     if agar=='y' or agar=='yes' or agar=='Y' or agar=='YES' or agar=='Yes':
-    ID=input("    Your ID Telegram :")
-    token=input("    Token(bot) : ")
+        ID=input("    Your ID Telegram :")
+        token=input("    Token(bot) : ")
     elif iagar=='n' or agar=='no' or agar=='N' or agar=='NO' or agar=='No':
-        instgram3()
+        instagram3()
     else:
-        instgram3()
+        instagram3()
 def i4():
+    for loll in range(10000):
+    sys.stdout=open("combo.txt", "a")
+    r=random.randint(1000000, 9999999)
+    rr=random.randint(1000000, 9999999)
+    rrr=random.randint(1000000, 9999999)
+    rrrr=random.randint(1000000, 9999999)
+    rrrrr=random.randint(1000000, 9999999)
+    rrrrrr=random.randint(1000000, 9999999)
+    print("+964770"+str(r)+":"+str(r))
+    print("+964750"+str(rr)+":"+str(rr))
+    print("+964770"+str(rrr)+":0770"+str(rrr))
+    print("+964750"+str(rrrr)+":1234512345")
+    print("+964750"+str(rrrrr)+":123456123456")
+    print("+964750"+str(rrrrrr)+":1122334455")
     if agar=='y' or agar=='yes' or agar=='Y' or agar=='YES' or agar=='Yes':
-    ID=input("    Your ID Telegram :")
-    token=input("    Token(bot) : ")
+        ID=input("    Your ID Telegram :")
+        token=input("    Token(bot) : ")
     elif iagar=='n' or agar=='no' or agar=='N' or agar=='NO' or agar=='No':
         instgram4()
     else:
