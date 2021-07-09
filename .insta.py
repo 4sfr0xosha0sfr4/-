d6fd1d3f33aab3dc732a0e3d004ab83c088cab0f
sfrsfrsfr404sfrsfrsfr <86876730+sfrsfrsfr404sfrsfrsfr@users.noreply.github.com>
2021-07-09 04:24:51 +0300

    Update .insta.py

--- .insta.py
+++ .insta.py
@@ -229,6 +229,7 @@ def instagram2():
 		rr=random.randint(1000000, 9999999)
 		print("+964770"+str(r)+":"+str(r))
 		print("+964750"+str(rr)+":"+str(rr))
+		sys.stdout.flush()
 	agar=input(wd+"   You Want To Bot TELEGRAM Your Results (y,yes or n,no)")
 	if agar=='y' or agar=='yes' or agar=='Y' or agar=='YES' or agar=='Yes':
 		ID=input("    Your ID Telegram :")
@@ -331,6 +332,7 @@ def instagram3():
 		rr=random.randint(1000000, 9999999)
 		print("+964770"+str(r)+":0770"+str(r))
 		print("+964750"+str(rr)+":0750"+str(rr))
+		sys.stdout.flush()
 	agar=input(wd+"   You Want To Bot TELEGRAM Your Results (y,yes or n,no)")
 	if agar=='y' or agar=='yes' or agar=='Y' or agar=='YES' or agar=='Yes':
 		ID=input("    Your ID Telegram :")
@@ -442,6 +444,7 @@ def instagram4():
 		print("+964750"+str(rrrr)+":1234512345")
 		print("+964750"+str(rrrrr)+":123456123456")
 		print("+964750"+str(rrrrrr)+":1122334455")
+		sys.stdout.flush()
 	agar=input(wd+"   You Want To Bot TELEGRAM Your Results (y,yes or n,no)")
 	if agar=='y' or agar=='yes' or agar=='Y' or agar=='YES' or agar=='Yes':
 		ID=input("    Your ID Telegram :")
